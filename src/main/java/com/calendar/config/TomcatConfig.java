930c025ec628bb0aa0e95ccf59ffd066768bf61c
weiming <weiming.wei@feisu.com>
2023-05-18 17:06:46 +0800

    配置ssl http和https均可访问

--- src/main/java/com/calendar/config/TomcatConfig.java
+++ src/main/java/com/calendar/config/TomcatConfig.java
@@ -1,2 +1,30 @@
-package com.calendar.config;public class TomcatConfig {
+package com.calendar.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * SSL部署配置，同时开放http和https访问
+ */
+@Configuration
+public class TomcatConfig {
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createHTTPConnector());
+        return tomcat;
+    }
+
+    private Connector createHTTPConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        //同时启用http（8082）、https（8081）两个端口
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setPort(80);
+        connector.setRedirectPort(443);
+        return connector;
+    }
 }
